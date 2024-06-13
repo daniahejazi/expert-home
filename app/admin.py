6def1dd6cd7572a70b03b24d8aa95392aecdd943
mohamedElbalky <mohammed4elbalky@gmail.com>
2024-06-13 21:52:00 +0300

    handle send emails, fix html error, add custom admin panel them and configur postgres as database

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User, Group
 
 from .models import Expert, Cerificate, Vocation
 
@@ -7,21 +7,69 @@ from .models import Expert, Cerificate, Vocation
 # admin.site.register(Expert)
 # admin.site.register(Cerificate)
 
+admin.site.site_title = "UHX Dashboard"
+admin.site.site_header = "UHX Dashboard"
+admin.site.index_title = "لوحة التحكم"
+
+
 class CerificateInline(admin.TabularInline):
     model = Cerificate
-    extra = 1
-    
+    extra = 0
+    fields = ("attachment",)
+    readonly_fields = ["attachment"]
+    can_delete = False
 
 
 @admin.register(Expert)
 class ExpertAdmin(admin.ModelAdmin):
-    list_display = ('created_at','name', 'general_specialization', "sum_experience_years")
-    search_fields = ('name', 'general_specialization')
-    list_filter = ('general_specialization', 'sum_experience_years')
-    list_per_page = 2
-    ordering = ('-created_at',)
-    
+    list_display = (
+        "created_at",
+        "name",
+        "general_specialization",
+        "sum_experience_years",
+    )
+    search_fields = ("name", "general_specialization")
+    list_filter = ("created_at",)
+    list_per_page = 20
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+    readonly_fields = (
+        "name",
+        # "email",
+        # "phone",
+        "general_specialization",
+        "sum_experience_years",
+        "details_specialization_info",
+        "display_name_type",
+        "services",
+        "work_experiense_comapnies",
+        "study_qualifications",
+        "heigh_special_companies",
+        "extraInfo",
+        "created_at",
+    )
+    list_display_links = ("created_at", "name")
+
     inlines = [CerificateInline]
 
 
-admin.site.register(Vocation)
+@admin.register(Vocation)
+class VocationAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "first_name", "last_name", "email", "phone")
+    readonly_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "message",
+        "created_at",
+    )
+    search_fields = ("first_name", "last_name", "email", "phone", "message")
+    list_filter = ("created_at",)
+    date_hierarchy = "created_at"
+    list_display_links = ("created_at", "first_name", "last_name")
+    list_per_page = 20
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
--- app/views.py
+++ app/views.py
@@ -1,89 +1,175 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponseRedirect
 from django.contrib import messages
 
-from .models import Cerificate, Expert, Vocation
+
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+
+from django.conf import settings
+from django.urls import reverse
+
+from .models import Cerificate, Expert
 
 # Create your views here.
 
 from .forms import VocationForm, ExpertForm
 
+
 def home_view(request):
-    return render(request, 'app/home.html')
+    return render(request, "app/home.html")
+
 
 def about_view(request):
-    return render(request, 'app/about.html')
+    return render(request, "app/about.html")
+
 
 def experience_view(request):
-    return render(request, 'app/experience.html')
+    return render(request, "app/experience.html")
+
 
 def licenses_view(request):
-    return render(request, 'app/licenses.html')
+    return render(request, "app/licenses.html")
+
 
 def services_view(request):
-    return render(request, 'app/services.html')
+    return render(request, "app/services.html")
+
 
 def action_aechanism_view(request):
-    return render(request, 'app/AechanismofAction.html')
+    return render(request, "app/AechanismofAction.html")
+
 
 def join_as_expert_view(request):
-    return render(request, 'app/JoinAsAnOfficer.html')
+    return render(request, "app/JoinAsAnOfficer.html")
 
 
 def join_as_expert_form_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         new_form = ExpertForm(request.POST)
         if new_form.is_valid():
             new_form.save()
-        
-            new_expert = Expert.objects.last()
             
+            new_expert = Expert.objects.last()
+
             if request.FILES:
                 for file in request.FILES.getlist("attachments"):
                     new_cerificate = Cerificate(expert=new_expert, attachment=file)
                     new_cerificate.save()
-                    messages.success(request, "تم حفظ بياناتك بنجاح")
-                    return redirect('join_as_officer')
-        
+                    
+                    
+            name = new_form.cleaned_data["name"]
+            general_specialization = new_form.cleaned_data["general_specialization"]
+            sum_experience_years = new_form.cleaned_data["sum_experience_years"]
+            
+            admin_url = reverse("admin:index")
+            admin_page_url = request.build_absolute_uri(admin_url)
 
-            # messages.success(request, "تم ارسال طلبك بنجاح")
-            # return redirect('join_as_officer')
+            context = {
+                "name": name,
+                "general_specialization": general_specialization,
+                "sum_experience_years": sum_experience_years,
+                "admin_page_url": admin_page_url,
+            }
+            email_body = render_to_string("component/join_email.html", context)
+
+            subject = f"طلب انضمام كخبير من السيد : {name}"
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = ["mohammed4elbalky@gmail.com"]
+
+            email = EmailMessage(subject, email_body, from_email, recipient_list)
+            email.content_subtype = "html"
+            email.send()
+
+            messages.success(request, "تم حفظ بياناتك بنجاح")
+            return redirect("join_as_officer")
         
+            
+
     messages.error(request, "يوجد خطآ برجاء اعادة المحاولة")
-    return redirect('join_as_officer')
-
-
-
-
+    return redirect("join_as_officer")
 
 
 def vocation_request_view(request):
-    return render(request, 'app/consultation_request.html')
+    return render(request, "app/consultation_request.html")
+
 
 def vocation_request_form_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
 
         form = VocationForm(request.POST)
-        print(form.data)
+        # print(form.data)
         if form.is_valid():
             form.save()
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            phone = form.cleaned_data["phone"]
+            message = form.cleaned_data["message"]
+
+            admin_url = reverse("admin:index")
+            admin_page_url = request.build_absolute_uri(admin_url)
+            
+            context = {
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "phone": phone,
+                "message": message,
+                "admin_page_url": admin_page_url,
+            }
+            email_body = render_to_string("component/vocation_email.html", context)
+
+            subject = f"طلب استشارة من السيد :{first_name} {last_name}"
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = ["mohammed4elbalky@gmail.com"]
+
+            email = EmailMessage(subject, email_body, from_email, recipient_list)
+            email.content_subtype = "html"
+            email.send()
+
             messages.success(request, "تم ارسال طلبك بنجاح")
-            return redirect('consultation_request')
-        
+            return redirect("consultation_request")
+
     messages.error(request, "يوجد خطآ برجاء اعادة المحاولة")
-    return redirect('consultation_request')
+    return redirect("consultation_request")
 
 
 def vocation_request_form_from_home_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
 
         form = VocationForm(request.POST)
-        print(form.data)
         if form.is_valid():
-            print("-------- OK -------")
             form.save()
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            phone = form.cleaned_data["phone"]
+            message = form.cleaned_data["message"]
+
+            admin_url = reverse("admin:index")
+            # Build the absolute URL
+            admin_page_url = request.build_absolute_uri(admin_url)
+            print(admin_page_url)
+            context = {
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "phone": phone,
+                "message": message,
+                "admin_page_url": admin_page_url,
+            }
+            email_body = render_to_string("component/vocation_email.html", context)
+
+            subject = f"طلب استشارة من السيد :{first_name} {last_name}"
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = ["mohammed4elbalky@gmail.com"]
+
+            email = EmailMessage(subject, email_body, from_email, recipient_list)
+            email.content_subtype = "html"
+            email.send()
+
             messages.success(request, "تم ارسال طلبك بنجاح")
-            return redirect('home')
-        
+            return redirect("home")
+
     messages.error(request, "يوجد خطآ برجاء اعادة المحاولة")
-    return redirect('home')
+    return redirect("home")
